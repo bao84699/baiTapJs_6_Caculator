47de11eb256479e4b935d2ecf63a96485850d09a
bao84699 <bao84699@gmail.com>
2026-01-08 21:11:30 +0700

    v02

--- script.js
+++ script.js
@@ -2,20 +2,35 @@ let display = document.getElementById("display");
 let button = document.querySelectorAll(".button");
 const sound = new Audio("Pictures/light-switch-button-epic-stock-media-1-00-00.mp3");
 let chuoinho = "";
+let kt = 0;
 
 button.forEach((bt)=>{
     bt.addEventListener("click",()=> {
         sound.play();
         switch(bt.innerText){
-            case "AC": chuoinho = ""; break;
+            case "AC": chuoinho = ""; kt = 0; break;
             case "DEL": chuoinho = chuoinho.slice(0,-1); break; 
             // --> Cắt vị trí cuôi
-            case "x": chuoinho += "*"; break;
-            case ",": chuoinho += "."; break;
-            case "=": chuoinho = Number(eval(chuoinho).toFixed(2)); break;
+            case "+/-":
+                if (chuoinho !== "") {
+                    if (chuoinho.startsWith("(-") && chuoinho.endsWith(")")) {
+                        chuoinho = chuoinho.slice(2, -1);
+                    } 
+                    else {
+                        chuoinho = "(-" + chuoinho + ")";
+                    }
+                }
+                break;
+            case "x": chuoinho += "*"; kt = 0; break;
+            case ",": chuoinho += "."; kt = 0; break;
+            case "=": chuoinho = Number(eval(chuoinho).toFixed(2));kt = 1;  break;
             // Làm tròn 2 chữ số và bỏ số 0 (nếu có)
             default:
+                if (kt === 1) {
+                    chuoinho = "";
+                }
                 chuoinho += bt.innerText;
+                kt = 0;
         }
         display.textContent = chuoinho;
     })
